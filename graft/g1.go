47e37b4525427fe1d7262b5c99aea134ae194071
birdream <bird.dream.norman@gmail.com>
2020-04-26 22:46:46 +0800

    add graft

---
+++ graft/g1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var set = make(map[int]bool, 0)
+
+func printOnce(num int) {
+	if _, ok := set[num]; !ok {
+		fmt.Println(num)
+	}
+
+	set[num] = true
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		go printOnce(100)
+	}
+	time.Sleep(time.Second)
+}
